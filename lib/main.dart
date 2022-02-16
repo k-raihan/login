8f25667a7bd5f49ece2d11dc5a088a5be0278450
Faysalsheikh <faysalsheikh1107@gmail.com>
2022-02-16 11:08:13 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'switch.dart';
+
+void main() {
+  runApp(const SwitchWidget());
+}
---
+++ lib/switch.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+class SwitchWidget extends StatefulWidget {
+  const SwitchWidget({ Key? key }) : super(key: key);
+
+  @override
+  _SwitchWidgetState createState() => _SwitchWidgetState();
+}
+
+class _SwitchWidgetState extends State<SwitchWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Switch Example"),
+        ),
+        body: Today(),
+      ),
+    );
+  }
+}
+class Today extends StatefulWidget {
+  SwitchClass createState() => new SwitchClass();
+}
+
+class _TodayState extends State<Today> {
+  bool isSwitched = false;
+  var textValue = "Switch is off";
+  void toggleSwitch(bool value){
+    if(isSwitched == false){
+      setState(() {
+        isSwitched = true;
+        textValue = "Switch Button is On";
+      });
+      print("Switch Button is ON");
+    }
+    else{
+      setState(() {
+        isSwitched = false;
+        textValue = "Switch Button is OFF";
+      });
+      print("Switch Button is OFF");
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Transform.scale(scale: 2,
+        child: Switch( onChanged: toggleSwitch,
+        value: isSwitched,
+        activeColor: Colors.blue,
+        activeTrackColor: Colors.yellow,
+        inactiveThumbColor: Colors.green,
+        inactiveTrackColor: Colors.red,
+        ),
+        ),
+        Text(
+          "$textValue",
+          style: TextStyle(fontSize: 20,
+        )
+        )],
+    );
+  }
+}
